f8ba5cecb71e458d9b43489c12dd8df63b718ece
peterhomoki <90681553+peterhomoki@users.noreply.github.com>
2023-05-06 21:32:41 +0200

    javítások 2023.05.06

--- Whisper_convert.py
+++ Whisper_convert.py
@@ -1,4 +1,7 @@
 import whisper
+import util
+import main
+import os
 
 accepted_extensions = "*.mp3;*.m4a;*.mpga;*.wav;*.webm"
 accepted_extensions_list = accepted_extensions.split(";")
@@ -12,7 +15,15 @@ def whisper_convert(input_file: str, output_file: str):
     """
 
 	modelName = "large-v2"
-	model = whisper.load_model(modelName)
-	result = model.transcribe(audio=input_file, word_timestamps=False, verbose=True)
-	with open(output_file, "w", encoding="utf-8") as file:
-		file.write(result["text"])
+	if util.check_split_files(input_file):
+		chunk_filenames = util.list_split_files(input_file) #ordered list of files is returned
+		main.output_queue.put("Converting multiple files ({})... \n".format(len(chunk_filenames)))
+		for filename in chunk_filenames:
+			model = whisper.load_model(modelName)
+			main.output_queue.put("Converting file: ({})... \n".format(os.path.basename(filename)))
+			result = model.transcribe(audio=file, word_timestamps=False, verbose=True)
+			with open(output_file, "a", encoding="utf-8") as file:
+				file.write(result["text"].strip())       
+	else:
+		with open(output_file, "w", encoding="utf-8") as file:
+			file.write(result["text"])
--- util.py
+++ util.py
@@ -3,6 +3,8 @@ import tiktoken
 import redact_text_w_openAI
 import json
 import logging
+from typing import List
+import os
 
 def own_prompt_length() -> int:
     size = return_token_length(redact_text_w_openAI.SYSTEM_PROMPT)
@@ -15,7 +17,7 @@ def return_token_length(sentence: str) -> int:
     encoding = tiktoken.get_encoding("cl100k_base")
     return len(encoding.encode(sentence))
     
-def create_chunks(text: str, max_chunk_size=8192) -> list[str]:
+def create_chunks(text: str, max_chunk_size=8192) -> List[str]:
     max_chunk_size=max_chunk_size-own_prompt_length()
     sentences = re.split(r'(?<=[.!?]) +', text)
 
@@ -40,3 +42,34 @@ def create_chunks(text: str, max_chunk_size=8192) -> list[str]:
         logging.info(f"chunksize: {return_token_length(current_chunk.strip())}")
 
     return chunks
+
+def check_split_files(input_file: str)-> bool:
+    filename_root, file_ext = os.path.splitext(input_file)
+    if os.path.exists(filename_root+"_0"+file_ext):
+        return True
+    else:
+        return False
+    
+def list_split_files(input_file: str)-> List[str]:
+    def number_from_filename(filename):
+        match = re.match(pattern, filename)
+        if match:
+            return int(match.group(1))
+        return None
+    
+    pattern = input_file + "_" 
+    matching_files = []
+    for file in os.listdir():
+        match = re.match(pattern, file)
+        matching_files.append(file)
+    matching_files.sort(key=number_from_filename)
+    result_files = []    
+    previous_number = None
+    for file in matching_files:
+        current_number = number_from_filename(file)
+        if previous_number is None or current_number == previous_number + 1:
+            result_files.append(file)
+            previous_number = current_number
+        else:
+            break
+    return result_files
